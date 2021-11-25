8190c5b0c7e4eb52d85d89b8f43e86f53abd9970
MiguelGalp <miguel.galperin@gmail.com>
2021-11-25 09:06:42 -0300

    otra animacion titulo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { IconContext } from 'react-icons'
-import { CSSTransitionGroup } from 'react-transition-group'
+
 
 
 
